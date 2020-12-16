25f2275c0a688d219eb5076ffbd6106ebbe22d99
cc743 <ca.flores349@gmail.com>
2020-12-16 18:26:14 -0500

    Task 4.3 unit testing

---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Event from '../Event';
+//import { mockData } from '../mock-data';
+
+describe ('<Event /> Component', () => {
+  const event = {
+    "kind": "calendar#event",
+    "etag": "\"3181161784712000\"",
+    "id": "4eahs9ghkhrvkld72hogu9ph3e_20200519T140000Z",
+    "status": "confirmed",
+    "htmlLink": "https://www.google.com/calendar/event?eid=NGVhaHM5Z2hraHJ2a2xkNzJob2d1OXBoM2VfMjAyMDA1MTlUMTQwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
+    "created": "2020-05-19T19:17:46.000Z",
+    "updated": "2020-05-27T12:01:32.356Z",
+    "summary": "Learn JavaScript",
+    "description": "Have you wondered how you can ask Google to show you the list of the top ten must-see places in London? And how Google presents you the list? How can you submit the details of an application? Well, JavaScript is doing these. :) \n\nJavascript offers interactivity to a dull, static website. Come, learn JavaScript with us and make those beautiful websites.",
+    "location": "London, UK",
+    "creator": {
+     "email": "fullstackwebdev@careerfoundry.com",
+     "self": true
+    },
+    "organizer": {
+     "email": "fullstackwebdev@careerfoundry.com",
+     "self": true
+    },
+    "start": {
+     "dateTime": "2020-05-19T16:00:00+02:00",
+     "timeZone": "Europe/Berlin"
+    },
+    "end": {
+     "dateTime": "2020-05-19T17:00:00+02:00",
+     "timeZone": "Europe/Berlin"
+    },
+    "recurringEventId": "4eahs9ghkhrvkld72hogu9ph3e",
+    "originalStartTime": {
+     "dateTime": "2020-05-19T16:00:00+02:00",
+     "timeZone": "Europe/Berlin"
+    },
+    "iCalUID": "4eahs9ghkhrvkld72hogu9ph3e@google.com",
+    "sequence": 0,
+    "reminders": {
+     "useDefault": true
+    }
+   }
+
+  let EventWrapper;
+  beforeAll(() => {
+    EventWrapper = shallow(<Event event={event}/>); //review this line of code, consider adding const event up above within describe() method
+  });
+
+  test('render the Event component', () => {
+    expect(EventWrapper).toHaveLength(1);
+  });
+
+  test('render the event div/element', () => {
+    expect(EventWrapper.find('.event')).toHaveLength(1);
+  });
+
+  test('render the event overview div', () => {
+    expect(EventWrapper.find('.event-overview')).toHaveLength(1);
+  });
+
+  test('render the event-details div', () => {
+    EventWrapper.setState({showDetails: true});
+    expect(EventWrapper.find('.event-details')).toHaveLength(1);
+  });
+
+  test('render the contents of the event overview', () => {
+    expect(EventWrapper.find('.event-overview').children()).toHaveLength(4);
+  });
+
+  test('render contents of the event-details', () => {
+    EventWrapper.setState({showDetails: true});
+    expect(EventWrapper.find('.event-details').children()).toHaveLength(4);
+  })
+
+  test('render the details button', () => {
+    expect(EventWrapper.find('.details-button')).toHaveLength(1);
+  }); 
+
+  test('expanding event details when button is clicked', () => {
+    EventWrapper.setState({showDetails: false});
+    EventWrapper.find('.details-button').simulate('click');
+    expect(EventWrapper.state('showDetails')).toBe(true);
+  });
+
+  test('collapsing event details when button is clicked', () => {
+    EventWrapper.setState({showDetails: true});
+    EventWrapper.find('.details-button').simulate('click');
+    expect(EventWrapper.state('showDetails')).toBe(false);
+  });
+
+})
---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NumberOfEvents from '../NumberOfEvents';
+
+describe('<NumberOfEvents /> Component', () => {
+  let NumberOfEventsWrapper;
+  beforeAll(() => {
+    NumberOfEventsWrapper = shallow(<NumberOfEvents />) 
+  });
+
+  test('render the NumberOfEventsComponent', () => {
+    expect(NumberOfEventsWrapper).toHaveLength(1);
+  });
+
+  test('render number-of-events element', () => {
+    expect(NumberOfEventsWrapper.find('.number-of-events')).toHaveLength(1);
+  });
+
+  test('render number-of-events-label', () => {
+    expect(NumberOfEventsWrapper.find('.number-of-events-label')).toHaveLength(1);
+  });
+
+  test('renders the text input correctly', () => {
+    const numberOfEvents = NumberOfEventsWrapper.state('numberOfEvents');
+    expect(NumberOfEventsWrapper.find('.event-number-input').prop('value')).toBe(numberOfEvents);
+  });
+
+  test('change state when the text input changes', () => {
+    NumberOfEventsWrapper.setState({
+      numberOfEvents: 32,
+    });
+    const eventObject = { target: {value: 10} };
+    NumberOfEventsWrapper.find('.event-number-input').simulate('change', eventObject);
+    expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe(10);
+  });
+
+});
