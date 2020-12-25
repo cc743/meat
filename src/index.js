7e5e82433fcc76d66e94723303f164af27304b88
cc743 <ca.flores349@gmail.com>
2020-12-25 18:50:18 -0500

    Task 4.8 up to serviceWorker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 import * as atatus from 'atatus-spa';
 
 ReactDOM.render(
@@ -17,3 +18,4 @@ ReactDOM.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 atatus.config('80e50b58135c40718afae6affd29f14c').install(); 
+serviceWorker.register();
