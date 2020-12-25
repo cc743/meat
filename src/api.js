6a2569a462ecc2a25398983c3310ca7f3ffadb12
cc743 <ca.flores349@gmail.com>
2020-12-24 21:21:15 -0500

    api.js update - unexpected string concatenation

--- src/api.js
+++ src/api.js
@@ -35,7 +35,7 @@ const removeQuery = () => {
 const getToken = async (code) => {
   const encodeCode = encodeURIComponent(code);
   const { access_token } = await fetch(
-    'https://6vwfp84687.execute-api.us-east-2.amazonaws.com/dev/api/token' + '/' + encodeCode
+    `https://6vwfp84687.execute-api.us-east-2.amazonaws.com/dev/api/token/${encodeCode}`
   )
     .then((res) => {
       return res.json();
@@ -59,7 +59,7 @@ export const getEvents = async () => {
 
   if (token) {
     removeQuery();
-    const url = 'https://6vwfp84687.execute-api.us-east-2.amazonaws.com/dev/api/get-events' + '/' + token;
+    const url = `https://6vwfp84687.execute-api.us-east-2.amazonaws.com/dev/api/get-events/${token}`;
     const result = await axios.get(url);
     if (result.data) {
       var locations = extractLocations(result.data.events);
